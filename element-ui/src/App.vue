9907d428d5f19049b632ceb3c89a5d74edd071cf
tanglin <2196184435@qq.com>
2018-06-25 21:34:02 +0800

    vue-element-ui手动实现

---
+++ element-ui/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <el-button :disabled="disabled" type="primary">
+      primary
+    </el-button>
+    <el-button type="success" loading>
+      success
+    </el-button>
+    <el-button :disabled="disabled" type="danger">
+      danger
+    </el-button>
+    <el-button type="info">
+      info
+    </el-button>
+    <el-button :disabled="disabled" type="warning">
+      warning
+    </el-button>
+    <el-button type="" icon="el-icon-edit" :loading="isLoading" @click="changeStatus">
+    </el-button>
+    <el-button type="" icon="el-icon-delete" >
+      icon-delete
+    </el-button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import ElButton from '@/components/ElButton'
+
+export default {
+  name: 'App',
+  components: {
+    'el-button': ElButton,
+  },
+  data() {
+    return {
+      disabled: false,
+      isLoading: false,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.disabled = true
+    }, 2000)
+
+  },
+  methods: {
+    changeStatus() {
+      alert('1')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ element-ui/src/components/ElButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <button class="el-button" :disabled="buttonDisabled" :type="buttonType" :class="[
+            type ? 'el-button--' + type : '',
+            {'is-disabled': buttonDisabled,
+            'is-loading': loading,
+            }
+        ]">
+        <i class="el-icon-loading" v-if="loading"></i>
+        <i :class="icon" v-if="icon&&!loading"></i>
+        <slot></slot>
+    </button>
+    <!-- 两种写法： 
+                三元运算符：disabled ? 'is-disabled' : '',
+                对象： {'is-disabled': buttonDisabled}
+     -->
+</template>
+
+<script>
+export default {
+  name: "ElButton",
+  props: {
+    disabled: Boolean,
+    type: {
+        type: String,
+        default: 'default'
+    },
+    loading: Boolean,
+    icon: {
+        icon: String,
+        default: ''
+    },
+  },
+  computed: {
+    buttonDisabled() {
+      return this.disabled
+    },
+
+    buttonType() {
+        return this.type
+    },
+
+  }
+};
+</script>
+
+<style>
+</style>
