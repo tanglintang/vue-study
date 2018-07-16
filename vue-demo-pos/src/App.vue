8e4fcdd53a74a52883375f1cb82a0e83ebea0a1c
tanglin <2196184435@qq.com>
2018-07-16 22:08:51 +0800

    vue-demo-pos银行收银系统

---
+++ vue-demo-pos/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <left-nav></left-nav>
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftNav from '@/components/LeftNav'
+
+export default {
+  name: 'App',
+  components: { LeftNav, }
+}
+</script>
+
+<style>
+.main {
+  float: left;
+  width: 95%;
+  height: 100%;
+  background-color: #eff2f7;
+  overflow: hidden;
+}
+</style>
---
+++ vue-demo-pos/src/views/Pos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        POS
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
