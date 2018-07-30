c4523e6c347006a71597859b763f21fccbaaf6fc
tanglin <2196184435@qq.com>
2018-07-30 23:53:52 +0800

    vue-组件递归-menuTree

---
+++ tree-menu/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <tree-menu :label="treeList.label" :nodes="treeList.nodes" :depth="0"/>
+  </div>
+</template>
+
+<script>
+import TreeMenu from "./components/TreeMenu";
+
+const list = {
+        label: "root",
+        nodes: [
+          {
+            label: "item 1",
+            nodes: [
+              {
+                label: "item 1.2",
+                nodes: [
+                  {
+                    label: "item 1.2.1"
+                  },
+                  {
+                    label: "item 1.2.2"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            label: "item 2",
+            nodes: [
+              {
+                label: "item 2.1",
+              }
+            ]
+          }
+        ]
+      }
+
+export default {
+  name: "App",
+  components: {
+    TreeMenu
+  },
+  data() {
+    return {
+      treeList: list
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ tree-menu/src/components/TreeMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tree-menu">
+    <div :style="indent">* {{label}}</div>
+    <tree-menu v-for="(node, index) in nodes" :nodes="node.nodes" :label="node.label" :key="index" :depth="depth + 1"></tree-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tree-menu',
+  props: ['label', 'nodes', 'depth'],
+  computed: {
+    indent () {
+      return { transform: `translate(${this.depth * 20}px)` }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
